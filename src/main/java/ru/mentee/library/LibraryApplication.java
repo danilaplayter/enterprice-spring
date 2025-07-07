8521c04d14c040d85523a4d4954550fd669787bc
Danila Plater <danilaplyter@gmail.com>
2025-07-07 09:43:11 +0300

    mp-191

--- src/main/java/ru/mentee/library/LibraryApplication.java
+++ src/main/java/ru/mentee/library/LibraryApplication.java
@@ -1,4 +1,15 @@
 /* @MENTEE_POWER (C)2025 */
 package ru.mentee.library;
 
-public class LibraryApplication {}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
--- src/main/java/ru/mentee/library/api/controller/BookController.java
+++ src/main/java/ru/mentee/library/api/controller/BookController.java
@@ -1,4 +1,64 @@
 /* @MENTEE_POWER (C)2025 */
 package ru.mentee.library.api.controller;
 
-public class BookController {}
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mentee.library.api.dto.BookDto;
+import ru.mentee.library.api.dto.CreateBookRequest;
+import ru.mentee.library.api.dto.UpdateBookRequest;
+import ru.mentee.library.service.BookService;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<BookDto> createBook(@Valid CreateBookRequest request) {
+        BookDto createdBook = bookService.create(request);
+        return ResponseEntity.created(URI.create("/api/books/" + createdBook.getId()))
+                .body(createdBook);
+    }
+
+    @GetMapping("/{id}")
+    public BookDto getBookById(@PathVariable long id) {
+        return bookService.getById(id);
+    }
+
+    @GetMapping
+    public List<BookDto> getAllBooks() {
+        return bookService.getAll();
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable long id, @Valid UpdateBookRequest request) {
+        return bookService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBook(@PathVariable long id) {
+        bookService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<BookDto> searchByAuthor(@RequestParam String author) {
+        return bookService.findByAuthor(author);
+    }
+}
